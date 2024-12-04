2e9d57fc100b37d3441b0b3ca94a4653be1fc71b
DoomDoggo64 <147098311+DoomDoggo64@users.noreply.github.com>
2024-12-04 16:13:47 +0000

    added more functions for testing

--- topylist/topylist.py
+++ topylist/topylist.py
@@ -21,12 +21,17 @@ def init_saves():
 
 def write_todo(saves,task,title):
     save =saves
+    for task in saves['tasks']['todo']:
+        if task['title'] == title:
+            print("Already a task with title: " + title)
+            return
     save['tasks']['todo'].append({'title':title, 'details':task,'date':datetime.date.today()})
     with open(PATH + '/save.txt','w') as f:
         f.write(str(saves))
 def print_off_tasks_todo(saves):
     for task in saves['tasks']['todo']:
-        print(task['details'])
+        #sorry future me for how unreadable. makes date obj a str then spliting to seperate the ymds takes away year
+        print(f'{task['title']}: {"-".join(str(task['date']).split('-')[1:3])}\n   {task['details']}')
 def finish_task(saves,task_title):
     save =saves
     for task_id, tasks in enumerate(saves['tasks']['todo']):
@@ -35,22 +40,28 @@ def finish_task(saves,task_title):
             del(save['tasks']['todo'][task_id])
             break
     else:
-        print('No task with that name')
+        print('No task with title: '+task_title)
         return
     save['tasks']['done'].append(task)
     save['tasks_done'] += 1 
     with open(PATH + '/save.txt','w') as f:
         f.write(str(saves))
-
+def clear_finished_tasks(saves):
+    save =saves
+    save['tasks_done'] = 0
+    save['tasks']['done'] = []
+    with open(PATH + '/save.txt','w') as f:
+        f.write(str(saves))
 def main():
     if not os.path.exists('topylist/saves/save.txt'):  
         init_saves()
     saves = load_todos()
-    print(saves)
-    
-    #write_todo(saves,'this is a task','task')
+
+    #write_todo(saves,'just live another day','live')
+    write_todo(saves,'eat dinner today','eat')
     saves = load_todos()
-    #finish_task(saves,'task')
+    finish_task(saves,'live')
     print_off_tasks_todo(saves)
+    clear_finished_tasks(saves)
 if __name__ == '__main__':
     main()
