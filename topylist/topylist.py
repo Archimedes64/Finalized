96b98eb688e970c0f9de37e57520c32c480dea71
DoomDoggo64 <147098311+DoomDoggo64@users.noreply.github.com>
2024-12-04 18:51:46 +0000

    switched to json

--- topylist/topylist.py
+++ topylist/topylist.py
@@ -1,37 +1,35 @@
 import os
 import datetime
+import argparse
+import json
 PATH = 'topylist/saves'
 
-
+argparser= argparse.ArgumentParser(description='A simple todo list program')
 
 def load_todos():
-    
     with open(PATH + '/save.txt','r') as f:
-        info = f.read()
-        data = eval(info)
-    return data
-    pass
+        return json.load(f)
 
 def init_saves():
     os.makedirs(PATH,exist_ok=True)
-    f = open(PATH+'/save.txt','w')
-    name = input("What is your name: ")
-    f.write(str({ 'tasks' : {'todo':[],'done':[]} ,'name':name,'tasks_done':0}))
-    f.close()
+    data = {
+        'tasks': {'todo': [], 'done': []},
+        'name': input('whats your name?'),
+        'tasks_done': 0
+    }
+    with open(PATH +'/saves.json','w') as f:
+        json.dump(data,f)
 
 def write_todo(saves,task,title):
     save =saves
-    for task in saves['tasks']['todo']:
-        if task['title'] == title:
-            print("Already a task with title: " + title)
-            return
+    if any(task['title'] == title for task in saves['tasks']['todo']):
+        print('Already a task with that title')
+        return
     save['tasks']['todo'].append({'title':title, 'details':task,'date':datetime.date.today()})
     with open(PATH + '/save.txt','w') as f:
-        f.write(str(saves))
-def print_off_tasks_todo(saves):
-    for task in saves['tasks']['todo']:
-        #sorry future me for how unreadable. makes date obj a str then spliting to seperate the ymds takes away year
-        print(f'{task['title']}: {"-".join(str(task['date']).split('-')[1:3])}\n   {task['details']}')
+        json.dump(save,f)
+
+
 def finish_task(saves,task_title):
     save =saves
     for task_id, tasks in enumerate(saves['tasks']['todo']):
@@ -45,23 +43,35 @@ def finish_task(saves,task_title):
     save['tasks']['done'].append(task)
     save['tasks_done'] += 1 
     with open(PATH + '/save.txt','w') as f:
-        f.write(str(saves))
+        json.dump(save,f)
 def clear_finished_tasks(saves):
     save =saves
     save['tasks_done'] = 0
     save['tasks']['done'] = []
     with open(PATH + '/save.txt','w') as f:
-        f.write(str(saves))
+        json.dump(save,f)
 def main():
     if not os.path.exists('topylist/saves/save.txt'):  
         init_saves()
     saves = load_todos()
-
-    #write_todo(saves,'just live another day','live')
-    write_todo(saves,'eat dinner today','eat')
-    saves = load_todos()
-    finish_task(saves,'live')
-    print_off_tasks_todo(saves)
-    clear_finished_tasks(saves)
+    argparser.add_argument('-a','--add',action='store_true')
+    argparser.add_argument('-l','--list',action='store_true')
+    argparser.add_argument('-f','--finish',action='store_true')
+    argparser.add_argument('-lf','--list_finished',action='store_true')
+    argparser.add_argument('--reset',action='store_true')
+    
+    args  = argparser.parse_args()
+    if args.add:
+        title = input("Enter task title: ")
+        description = input("Task Description: ")
+        write_todo(saves,description,title)
+    elif args.list:
+        print_off_tasks_todo(saves)
+    elif args.list_finished:
+        print_off_tasks_done(saves)
+    elif args.finish:
+        finish_task(saves,task_title=input("Task title you finished: "))
+    elif args.reset:
+        init_saves()
 if __name__ == '__main__':
     main()
